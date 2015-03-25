1dca84a06ebd9995578a2268a3a16b9a410238d8
Joost Hietbrink <joost@joopp.com>
2015-03-25 16:52:05 +0100

    Initial v1 API wrapper.

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # YelloyelloApi
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/yelloyello_api`. To experiment with that code, run `bin/console` for an interactive prompt.
-
-TODO: Delete this and the text above, and describe your gem
+This package is in beta and for private use only. Everything might change :)
 
 ## Installation
 
@@ -22,7 +20,9 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+    client = YelloyelloApi::Client.new(username, password)
+    results = client.connection.get('places/search.json', search: {q: 'restaurant'})
+    results.body.places.first.name # => "Some name"
 
 ## Development
 
--- lib/yelloyello_api.rb
+++ lib/yelloyello_api.rb
@@ -1,5 +1,35 @@
 require "yelloyello_api/version"
 
+require 'hashie'
+require 'faraday_middleware'
 module YelloyelloApi
-  # Your code goes here...
+
+  class Client
+
+    attr_accessor :api_host, :api_version, :api_url, :api_key, :connection
+
+    def initialize(username, password)
+      @api_key = api_key
+      @api_host = 'www.yelloyello.com'
+      @api_version = 'v1'
+      @api_url = "http://#{@api_host}/api/#{@api_version}"
+      # More info: http://www.bjoernrochel.de/2013/02/09/oh-my-faraday/
+      @connection = Faraday.new(url: api_url, headers: { accept: 'application/json' }) do |builder|
+        builder.adapter   Faraday.default_adapter
+        builder.basic_auth username, password
+        # builder.request :basic_auth, username, password
+        builder.params = { auth_token: api_key }
+
+        builder.request   :json
+        # builder.request   :basic_auth, options[:user], options[:password]
+        builder.request   :retry
+
+        builder.response  :mashify # Order seems important here :S
+        builder.response  :json, :content_type => /\b(json|json-home)$/
+      end
+      @connection
+    end
+
+  end # /Client
+
 end
